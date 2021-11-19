fcc5abd04d0196b37dbdd11267791cb47ffde081
Trezy <t@trezy.com>
2021-11-19 12:04:34 -0600

    build(rollup): stop compiling esm files

--- packages/grammars/rollup.config.js
+++ packages/grammars/rollup.config.js
@@ -1,33 +1,49 @@
 // Module imports
 import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
-import resolve from '@rollup/plugin-node-resolve'
 import external from 'rollup-plugin-peer-deps-external'
+import fs from 'fs'
+import path from 'path'
+import resolve from '@rollup/plugin-node-resolve'
 
 
 
 
+
+// Constants
+const plugins = [
+	babel({ exclude: 'node_modules' }),
+	external(),
+	commonjs(),
+	resolve(),
+]
+
+
+
+
+
+function getGrammarFiles() {
+	const grammarsPath = path.join('.', 'src', 'grammars')
+	const grammarFiles = fs.readdirSync(grammarsPath)
+	return grammarFiles.reduce((accumulator, filename) => {
+		accumulator[path.join('grammars', filename.replace(/\.mjs$/, ''))] = path.join(grammarsPath, filename)
+		return accumulator
+	}, {})
+}
 
 export default [
 	{
-		input: './src/index.js',
+		input: {
+			'index': path.join('.', 'src', 'index.mjs'),
+			...getGrammarFiles(),
+		},
 		output: [
 			{
-				file: 'dist/index.js',
+				dir: 'dist',
+				entryFileNames: '[name].js',
 				format: 'cjs',
 			},
-			{
-				file: 'dist/index.mjs',
-				format: 'es',
-				exports: 'named',
-			},
 		],
-		external: ['prop-types'],
-		plugins: [
-			babel({ exclude: 'node_modules' }),
-			external(),
-			commonjs(),
-			resolve(),
-		],
-	}
+		plugins,
+	},
 ]
