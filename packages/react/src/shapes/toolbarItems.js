651ae4e1ad0cc4c37f276496a2943bd9d85379d5
Trezy <t@trezy.com>
2021-12-03 12:23:52 -0600

    fix(@prismoid/react): fix prop checking for toolbar items with options

--- packages/react/src/shapes/toolbarItems.js
+++ packages/react/src/shapes/toolbarItems.js
@@ -7,25 +7,21 @@ import PropTypes from 'prop-types'
 
 export const toolbarItemsShape = PropTypes.arrayOf(
 	PropTypes.oneOfType([
+		PropTypes.node,
 		PropTypes.string,
 		PropTypes.arrayOf(function (propValue, index, componentName, location, propFullName) {
 			const errorStringPrefix = `Invalid prop \`${propFullName}\` supplied to \`${componentName}\`.`
 
-			switch (index) {
-				case '0':
-					if (typeof propValue !== 'string') {
-						return new Error(`${errorStringPrefix} First argument must be a string. Validation failed.`)
-					}
-					break
+			if (typeof propValue[0] !== 'string') {
+				return new Error(`${errorStringPrefix} First argument must be a string. Validation failed.`)
+			}
 
-				case '1':
-					if (propValue && ((typeof propValue !== 'object') || Array.isArray(propValue))) {
-						return new Error(`${errorStringPrefix} Second argument must be an options object. Validation failed.`)
-					}
-					break
+			if (propValue[1] && ((typeof propValue[1] !== 'object') || Array.isArray(propValue[1]))) {
+				return new Error(`${errorStringPrefix} Second argument must be an options object. Validation failed.`)
+			}
 
-				default:
-					return new Error(`${errorStringPrefix} Arrays may only include 2 items. Validation failed.`)
+			if (propValue.length > 2) {
+				return new Error(`${errorStringPrefix} Arrays may only include 2 items. Validation failed.`)
 			}
 		}),
 	])
