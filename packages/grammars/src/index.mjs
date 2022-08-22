35fafde71e08e43a1f9996159268508b80946b1c
Trezy <t@trezy.com>
2022-08-22 10:19:51 -0500

    fix(grammars/protobuf): add protobuf to exports

--- packages/grammars/src/index.mjs
+++ packages/grammars/src/index.mjs
@@ -4,6 +4,7 @@ import { go } from './grammars/go.mjs'
 import { Grammar } from './structures/Grammar.mjs'
 import { GrammarManager } from './structures/GrammarManager.mjs'
 import { javascript } from './grammars/javascript.mjs'
+import { protobuf } from './grammars/protobuf.mjs'
 import { regex } from './grammars/regex.mjs'
 import { rust } from './grammars/rust.mjs'
 import aliases from './aliases.mjs'
@@ -14,6 +15,7 @@ export {
 	clike,
 	go,
 	javascript,
+	protobuf,
 	regex,
 	rust,
 
